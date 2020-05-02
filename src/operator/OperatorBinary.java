9e2fb0c3c97e4a150b170d1a7069ed4e105fd533
walia1400 <waliaarahmah12@gmail.com>
2020-05-02 17:19:44 +0800

    Membuat String dari array Char toUppercase untuk ... toLowercase untuk ... substring(5) untuk ... substring(0,4) untuk ...

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,12 @@
+package operator;
+
+public class OperatorBinary {
+    //operator (=) sebagai assignment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Walia Rahmah";
+    int jumlahSks = 24;
+    double indeksPrestasiKomulatif = 3.89;
+
+}
